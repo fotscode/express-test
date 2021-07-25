cb9b3232a97b91b070b2dd31a5e493028131349c
fotscode <facundotomatis@gmail.com>
2021-07-25 20:33:41 -0300

    added logout route

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ router.get("/login",user.show_login);
 router.get("/signup",user.show_signup);
 router.post("/login",user.login);
 router.post("/signup",user.signup);
+router.get("/logout",user.logout);
+router.post("/logout",user.logout);
 
 /* GET home page. */
 router.get('/', landing.get_landing);
